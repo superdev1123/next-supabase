a4358620bfa6a8ecb479e2dfe1b356c89ccf6983
Lightning <dear.lightning1123@gmail.com>
2025-06-05 10:58:02 -0700

    feat: CI/CD

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,3 @@
-import { NextLogo } from "./next-logo";
-import { SupabaseLogo } from "./supabase-logo";
-
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
