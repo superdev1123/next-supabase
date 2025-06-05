bc2f5e131b9406b85806d2b4f97f3c053b165323
Lightning <dear.lightning1123@gmail.com>
2025-06-05 10:40:11 -0700

    test deploy

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,16 +19,6 @@ export function Hero() {
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
         <a
           href="https://nextjs.org/"
           target="_blank"
